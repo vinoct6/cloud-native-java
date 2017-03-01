356cf263a09897bdcf9d6a507a4af882514347b7
Vinoth Kumar <vinoct6@gmail.com>
2017-03-01 08:12:55 +0530

    Add Eventual consistency for transactions

--- src/main/java/com/example/ReservationServiceApplication.java
+++ src/main/java/com/example/ReservationServiceApplication.java
@@ -9,10 +9,17 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,6 +32,7 @@ import javax.persistence.Id;
 import java.util.Collection;
 import java.util.stream.Stream;
 
+@EnableBinding(ReservationChannels.class)
 @SpringBootApplication
 @EnableDiscoveryClient
 //@EnableEurekaClient //Technology specific
@@ -34,6 +42,27 @@ public class ReservationServiceApplication {
         SpringApplication.run(ReservationServiceApplication.class, args);
     }
 }
+
+
+interface ReservationChannels {
+
+    @Input
+    SubscribableChannel input();
+}
+
+@MessageEndpoint
+class ReservationProcessor {
+
+    @Autowired
+    private ReservationRepository repository;
+
+    @ServiceActivator(inputChannel = "input")
+    public void acceptNewReservation(Message<String> msg) {
+        System.out.println(msg);
+        this.repository.save(new Reservation(msg.getPayload()));
+    }
+}
+
 
 @RestController
 @RefreshScope
